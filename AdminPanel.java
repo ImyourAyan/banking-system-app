677e7355840ff88e36bffde026c1778d55faa738
Ayan Banerjee <ayanbanerjee231@gmail.com>
2026-01-25 01:36:27 +0530

    Remove unwanted noise from AdminPanel.java source code

--- AdminPanel.java
+++ AdminPanel.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 public class AdminPanel {
     public static void main(String[] args) {
-        System.out.println("Admin Panel: Access Granted");
+        System.out.println("Admin Panel: Access Granted:\n 1. View total users\n 2. View total transactions\n 3. View active users\n 4. View inactive users");
         // Additional admin functionalities can be implemented here
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
         
         switch (input) {
             case 1:
+                BankingSystem app = new BankingSystem();
+                ;
                 System.out.println("Total users number: ");
                 break;
             
@@ -21,7 +23,7 @@ public class AdminPanel {
             case 4:
                 System.out.println("Total inactive users number: ");
                 break;
-                
+
             default:
                 System.out.println("Invalid input");
                 break;
